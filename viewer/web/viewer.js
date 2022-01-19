c47dbaeb9cadb6fbbaa4d3a4c321c8fb37540c94
KiarashS <kiarash.s@hotmail.com>
2022-01-19 11:46:12 +0330

    update

--- viewer/web/viewer.js
+++ viewer/web/viewer.js
@@ -2168,7 +2168,7 @@ const PDFViewerApplication = {
 exports.PDFViewerApplication = PDFViewerApplication;
 let validateFileURL;
 {
-  const HOSTED_VIEWER_ORIGINS = ["null", "http://mozilla.github.io", "https://mozilla.github.io"];
+  const HOSTED_VIEWER_ORIGINS = ["null", "http://mozilla.github.io", "https://mozilla.github.io", "file://", "http://github.com", "https://github.com"];
 
   validateFileURL = function (file) {
     if (file === undefined) {
